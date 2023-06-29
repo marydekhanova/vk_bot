1746c6a33f758a0070192e7d5857e3d2d6684e08
Alexandr Tishkin <zuluwar6@gmail.com>
2023-06-29 18:02:23 +0300

    Fixed db_models

--- db_main.py
+++ db_main.py
@@ -1,14 +1,34 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-import psycopg2
-from models import create_tables
+#import psycopg2
+from db_models import create_tables
+from db_config import LOGIN, PASSWORD, DB_PORT, DB_NAME
 
-login = 'postgres'
-password = 'Danil367173'
-DSN = f'postgresql://{login}:{password}@localhost:5432/Bot_VK'
-engine = sqlalchemy.create_engine(DSN)
 
-create_tables(engine)
+#это временная заглушка, потом лишние точки входа уберем, просто пока сложновато
+if __name__ == '__main__':
+    DSN = f'postgresql://{LOGIN}:{PASSWORD}@localhost:{DB_PORT}/{DB_NAME}'
+    engine = sqlalchemy.create_engine(DSN)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    create_tables(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+def add_bot_user():
+    pass
+
+def add_vk_users():
+    pass
+
+def delete_vk_users():
+    pass
+
+def add_favourite():
+    pass
+
+def add_photos():
+    pass
+
+def add_blacklist():
+    pass
--- db_models.py
+++ db_models.py
@@ -6,38 +6,45 @@ Base = declarative_base()
 class Bot_user(Base):
     __tablename__ = "bot_user"
     bot_user_id = sq.Column(sq.Integer, primary_key=True)
+    #а нам действительно нужно поле никнейм? не совсем понимаю, когда будем выводить, но посмотрим
     nickname = sq.Column(sq.String(length=20), unique=True)
-    age = sq.Column(sq.Integer, nullable=False)
+    from_age = sq.Column(sq.Integer, nullable=False)
+    to_age = sq.Column(sq.Integer, nullable=False)
     gender = sq.Column(sq.String, nullable=False)
     city = sq.Column(sq.String, nullable=False)
+    offset = sq.Column(sq.Integer, nullable=False)
 
-class User_VK(Base):
-    __tablename__ = "user_VK"
-    user_VK_id = sq.Column(sq.Integer, primary_key=True)
-    name = sq.Column(sq.String(length=20), unique=True)
-    surname = sq.Column(sq.String(length=20), unique=True)
-    link_to_profile = sq.Column(sq.String(length=40), unique=True)
+
+class buffer_user_VK(Base):
+    __tablename__ = "buffer_user_VK"
+    user_id = sq.Column(sq.Integer, primary_key=True)
+    VK_id = sq.Column(sq.Integer, unique=True, nullable=False)
+    name = sq.Column(sq.String(length=20))
+    surname = sq.Column(sq.String(length=20))
+    link_to_profile = sq.Column(sq.String(length=40), unique=True, nullable=False)
     bot_user_id = sq.Column(sq.Integer, sq.ForeignKey("bot_user.bot_user_id"), nullable=False)
     bot_user = relationship(Bot_user, backref="users_VK")
 
 class Blacklist(Base):
     __tablename__ = "blacklist"
-    blacklist_user_id = sq.Column(sq.Integer, primary_key=True)
-    user_VK_id = sq.Column(sq.Integer, sq.ForeignKey("user_VK.user_VK_id"), nullable=False)
-    user_VK = relationship(User_VK, backref="blacklists")
+    blacklist_VK_id = sq.Column(sq.Integer, primary_key=True)
+    bot_user_id = sq.Column(sq.Integer, sq.ForeignKey("bot_user.bot_user_id"), nullable=False)
+    bot_user = relationship(Bot_user, backref="blacklists")
 
 class Favorite_VK(Base):
-    __tablename__ = "favorite_VK"
-    favorite_id = sq.Column(sq.Integer, primary_key=True)
-    user_VK_id = sq.Column(sq.Integer, sq.ForeignKey("user_VK.user_VK_id"), nullable=False)
-    user_VK = relationship(User_VK, backref="favorites_VK")
+    __tablename__ = "favourite_VK"
+    favourite_VK_id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=20))
+    surname = sq.Column(sq.String(length=20))
+    link_to_profile = sq.Column(sq.String(length=40), unique=True, nullable=False)
+    bot_user_id = sq.Column(sq.Integer, sq.ForeignKey("bot_user.bot_user_id"), nullable=False)
+    bot_user = relationship(Bot_user, backref="favourites")
 
 class Photo_VK(Base):
     __tablename__ = "photo_VK"
     photo_id = sq.Column(sq.Integer, primary_key=True)
-    media_id = sq.Column(sq.String(length=20), unique=True)
-    user_VK_id = sq.Column(sq.Integer, sq.ForeignKey("user_VK.user_VK_id"), nullable=False)
-    user_VK = relationship(User_VK, backref="photos_VK")
+    user_VK_id = sq.Column(sq.Integer, sq.ForeignKey("favourite_VK.favourite_VK_id"), nullable=False)
+    user_VK = relationship(Favorite_VK, backref="photos_VK")
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
